e18409d84116388227d439a240f700e21cd3acb7
ZanaSiaka <zanacoul210@gmail.com>
2025-02-05 10:41:10 +0000

    modif read

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-let frame;
+const windowWidth = innerWidth;
+const bodyHtml= document.getElementById('body');
+
+window.addEventListener('resize', () => {
+    if (bodyHtml < 799) {
+        body.style.display = 'none';
+    };
+    
+    console.log(windowWidth);
+    
+});
+console.log(windowWidth);
