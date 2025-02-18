a9a464dbad0a3c601614b9ffe6af001f431555ed
Odiivan643 <146976136+Odiivan643@users.noreply.github.com>
2025-02-18 10:30:33 +0000

    Marie orh

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title> Twin | Awards </title>
+  <link rel="icon shortcut" href="images/logoEsatic.png">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline text-red-600 text-center">
+    Ma fils Marie la sorcière !!
+  </h1>
+</body>
+</html>
